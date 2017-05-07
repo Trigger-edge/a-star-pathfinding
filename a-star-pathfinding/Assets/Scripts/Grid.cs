30f156253ca936b5d04dd3d1940e8db0ec68dfc3
Vivian Rajkumar <paintfrussianblue@gmail.com>
2017-05-07 13:29:41 +0800

    Added Initial A* Implementation

--- a-star-pathfinding/Assets/Scripts/Grid.cs
+++ a-star-pathfinding/Assets/Scripts/Grid.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Grid : MonoBehaviour {
 
@@ -12,13 +13,17 @@ public class Grid : MonoBehaviour {
 	int gridSizeX, gridSizeY;
 
 	void Start() {
+		
 		nodeDiameter = nodeRadius * 2;
 		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
 		gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+
 		CreateGrid ();
+
 	}
 
 	void CreateGrid() {
+		
 		grid = new Node[gridSizeX, gridSizeY];
 		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
 
@@ -28,14 +33,41 @@ public class Grid : MonoBehaviour {
 				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
 				bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
 
-				grid [x, y] = new Node (walkable, worldPoint);
+				grid [x, y] = new Node (walkable, worldPoint, x, y);
 			}
 		}
+
+	}
+
+	public List<Node> GetNeighbours (Node node) {
+
+		List<Node> neighbours = new List<Node> ();
+
+		// Loop
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				// Skip if node is same as current node
+				if (x == 0 && y == 0)
+					continue;
+
+				int checkX = node.gridX + x;
+				int checkY = node.gridY + y;
+
+				// Check if inside grid
+				if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
+					neighbours.Add (grid [checkX, checkY]);
+				}
+			}
+		}
+
+		return neighbours;
+
 	}
 
 	public Node GetNodeFromWorldPoint(Vector3 worldPosition) {
-		float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
-		float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+
+		float percentX = (worldPosition.x / gridWorldSize.x) + .5f;
+		float percentY = (worldPosition.z / gridWorldSize.y) + .5f;
 
 		// Clamp values between 0 and 1
 		percentX = Mathf.Clamp01 (percentX);
@@ -45,18 +77,28 @@ public class Grid : MonoBehaviour {
 		int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
 
 		return grid [x, y];
+
 	}
 
+	public List<Node> path;
 	void OnDrawGizmos() {
+		
 		Gizmos.DrawWireCube (transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
 		if (grid != null) {
 			foreach (Node n in grid) {
 				Gizmos.color = (n.walkable) ? Color.white : Color.red;
+
+				if (path != null) {
+					if (path.Contains (n)) {
+						Gizmos.color = Color.black;
+					}
+				}
+
 				Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
 			}
 		}
-	}
 
+	}
 
 }
---
+++ a-star-pathfinding/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pathfinding : MonoBehaviour {
+
+	public Transform seeker, target;
+	Grid grid;
+
+	void Awake() {
+		
+		grid = GetComponent<Grid> ();
+
+	}
+
+	void Update() {
+
+		FindPath (seeker.position, target.position);
+
+	}
+
+	void FindPath(Vector3 startPos, Vector3 targetPos) {
+
+		Node startNode = grid.GetNodeFromWorldPoint (startPos);
+		Node targetNode = grid.GetNodeFromWorldPoint (targetPos);
+
+		// open-set
+		List<Node> openSet = new List<Node>();
+
+		// closed-set
+		HashSet<Node> closedSet = new HashSet<Node>();
+
+		openSet.Add (startNode);
+
+		// Loop
+		while (openSet.Count > 0) {
+			Node currentNode = openSet [0];
+			for (int i = 1; i < openSet.Count; i++) {
+				if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost) {
+					currentNode = openSet[i];
+				}
+			}
+
+			openSet.Remove (currentNode);
+			closedSet.Add (currentNode);
+
+			// If path found, retrace path
+			if (currentNode == targetNode) {
+				RetracePath (startNode, targetNode);
+				return;
+			}
+
+			foreach (Node neighbour in grid.GetNeighbours(currentNode)) {
+				// If neighbour is unwalkable or already in closed-set, skip
+				if (!neighbour.walkable || closedSet.Contains (neighbour)) {
+					continue;
+				}
+					
+				int newCostToNeighbour = currentNode.gCost + GetDistance (currentNode, neighbour);
+
+				// If new path to neighbour is shorter or neighbour not in open-set
+				if (newCostToNeighbour < neighbour.gCost || !openSet.Contains (neighbour)) {
+					// Set neighbour's new f-cost
+					neighbour.gCost = newCostToNeighbour;
+					neighbour.hCost = GetDistance (neighbour, targetNode);
+
+					// Set parent of neighbour to current node
+					neighbour.parent = currentNode;
+
+					// If neighbour not in open-set, add it to open-set
+					if (!openSet.Contains (neighbour))
+						openSet.Add (neighbour);
+				}
+			}
+		}
+
+	}
+
+	void RetracePath(Node startNode, Node targetNode) {
+
+		// Retrace path backwards
+		List<Node> path = new List<Node> ();
+		Node currentNode = targetNode;
+
+		while (currentNode != startNode) {
+			path.Add (currentNode);
+			currentNode = currentNode.parent;
+		}
+
+		// Reverse path
+		path.Reverse ();
+
+		// Test using Gizmos
+		grid.path = path;
+
+	}
+
+	int GetDistance(Node nodeA, Node nodeB) {
+
+		// Heuristic
+		int distX = Mathf.Abs (nodeA.gridX - nodeB.gridX);
+		int distY = Mathf.Abs (nodeA.gridY - nodeB.gridY);
+
+		if (distX > distY)
+			return 14 * distY + 10 * (distX - distY);
+		
+		return 14 * distX + 10 * (distY - distX);
+
+	}
+
+}
